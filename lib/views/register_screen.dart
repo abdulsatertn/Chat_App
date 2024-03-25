d812061c0ac3f808217df2aaf31b23b5bd4febdb
abduustn <115055703+abduustn@users.noreply.github.com>
2024-03-25 08:19:01 -0700

    Refactor RegisterPage to use RegisterCubit for user registration
    Refactor RegisterPage to use RegisterCubit for user registration instead of handling
    it within the widget. This improves separation of concerns and follows best
    practices for state management.

--- lib/views/register_screen.dart
+++ lib/views/register_screen.dart
@@ -1,25 +1,20 @@
 import 'package:chat_app_second/constants.dart';
 import 'package:chat_app_second/helper/show_snack_bar.dart';
 import 'package:chat_app_second/views/chat_page.dart';
+import 'package:chat_app_second/views/cubits/register_cubit/register_cubit.dart';
 import 'package:chat_app_second/widgets/Custom_text_field.dart';
 import 'package:chat_app_second/widgets/custom_button.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
-class RegisterPage extends StatefulWidget {
-  RegisterPage({super.key});
-
-  static String id = 'registerPage';
-
-  @override
-  State<RegisterPage> createState() => _RegisterPageState();
-}
-
-class _RegisterPageState extends State<RegisterPage> {
+class RegisterPage extends StatelessWidget {
   String? email;
 
   String? password;
+
+  static String id = 'registerPage';
 
   bool isLoading = false;
 
@@ -27,128 +22,124 @@ class _RegisterPageState extends State<RegisterPage> {
 
   @override
   Widget build(BuildContext context) {
-    return ModalProgressHUD(
-      inAsyncCall: isLoading,
-      child: Scaffold(
-        backgroundColor: kPrimaryColor,
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8),
-          child: Form(
-            key: formKey,
-            child: ListView(
-              children: [
-                const SizedBox(height: 75),
-                Image.asset(
-                  'assets/images/scholar.png',
-                  height: 100,
-                ),
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+    return BlocConsumer<RegisterCubit, RegisterState>(
+      listener: (context, state) {
+        if (state is RegisterLoading) {
+          isLoading = true;
+        } else if (state is RegisterSuccess) {
+          isLoading = false;
+          Navigator.pushNamed(context, ChatPage.id);
+        } else if (state is RegisterFailure) {
+          isLoading = false;
+          showSnackBar(context, state.errorMessage);
+        }
+      },
+      builder: (context, state) {
+        return ModalProgressHUD(
+          inAsyncCall: isLoading,
+          child: Scaffold(
+            backgroundColor: kPrimaryColor,
+            body: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              child: Form(
+                key: formKey,
+                child: ListView(
                   children: [
-                    Text(
-                      'Scholar Chat',
-                      style: TextStyle(
-                        fontFamily: 'pacifico',
-                        color: Colors.white,
-                        fontSize: 32,
-                      ),
+                    const SizedBox(height: 75),
+                    Image.asset(
+                      'assets/images/scholar.png',
+                      height: 100,
                     ),
-                  ],
-                ),
-                const SizedBox(height: 50),
-                const Row(
-                  children: [
-                    Text(
-                      'Sign Up',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 24,
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                CustomFormTextField(
-                  onChanged: (data) {
-                    email = data;
-                  },
-                  hintText: 'Email',
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                CustomFormTextField(
-                  onChanged: (data) {
-                    password = data;
-                  },
-                  hintText: 'Password',
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                CustomButton(
-                  onTap: () async {
-                    if (formKey.currentState!.validate()) {
-                      isLoading = true;
-                      setState(() {});
-                      try {
-                        await registerUser();
-
-                        Navigator.pushNamed(
-                          context,
-                          ChatPage.id,
-                          arguments: email,
-                        );
-                      } on FirebaseAuthException catch (ex) {
-                        if (ex.code == 'weak-password') {
-                          showSnackBar(context, 'weak password');
-                        } else if (ex.code == 'email-already-in-use') {
-                          showSnackBar(context, 'The email is already signed.');
-                        }
-                      } catch (ex) {
-                        showSnackBar(context, 'there was an error');
-                      }
-                      isLoading = false;
-                      setState(() {});
-                    } else {}
-                  },
-                  title: 'SIGN UP',
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(
-                      'already have an account?  ',
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.pop(context);
-                      },
-                      child: const Text(
-                        '   sign in',
-                        style: TextStyle(
-                          color: Color(0xffC7EDE6),
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Scholar Chat',
+                          style: TextStyle(
+                            fontFamily: 'pacifico',
+                            color: Colors.white,
+                            fontSize: 32,
+                          ),
                         ),
-                      ),
-                    )
+                      ],
+                    ),
+                    const SizedBox(height: 50),
+                    const Row(
+                      children: [
+                        Text(
+                          'Sign Up',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    CustomFormTextField(
+                      onChanged: (data) {
+                        email = data;
+                      },
+                      hintText: 'Email',
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    CustomFormTextField(
+                      onChanged: (data) {
+                        password = data;
+                      },
+                      hintText: 'Password',
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    CustomButton(
+                      onTap: () async {
+                        if (formKey.currentState!.validate()) {
+                          BlocProvider.of<RegisterCubit>(context)
+                              .registerUser(email!, password!);
+                        } else {}
+                      },
+                      title: 'SIGN UP',
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          'already have an account?  ',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Text(
+                            '   sign in',
+                            style: TextStyle(
+                              color: Color(0xffC7EDE6),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                    const Spacer(
+                      flex: 3,
+                    ),
                   ],
                 ),
-                const Spacer(
-                  flex: 3,
-                ),
-              ],
+              ),
             ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 
