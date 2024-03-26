a5d318bbeaa6e347792c622e2f3cc9e91bbdcbb3
abduustn <115055703+abduustn@users.noreply.github.com>
2024-03-26 12:58:31 -0700

    Add authentication cubit with login and register functionality
    Added AuthCubit class with login and register user functions that interact
    with Firebase authentication. Also included AuthState class with initial
    states for authentication.

---
+++ lib/views/cubits/auth_cubit/auth_cubit.dart
@@ -0,0 +1,47 @@
+import 'package:bloc/bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:meta/meta.dart';
+
+part 'auth_state.dart';
+
+class AuthCubit extends Cubit<AuthState> {
+  AuthCubit() : super(AuthInitial());
+
+  Future<void> loginUser(
+      {required String email, required String password}) async {
+    emit(LoginLoading());
+    try {
+      UserCredential user = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+
+      emit(LoginSuccess());
+    } on FirebaseAuthException catch (ex) {
+      if (ex.code == 'wrong-password') {
+        emit(LoginFailure(errorMessage: 'wrong password'));
+      } else if (ex.code == 'user-not-found') {
+        emit(LoginFailure(errorMessage: 'user not found'));
+      }
+    } on Exception catch (e) {
+      emit(LoginFailure(errorMessage: 'something went wrong'));
+    }
+  }
+
+  Future<void> registerUser(String email, String password) async {
+    emit(RegisterLoading());
+
+    try {
+      UserCredential user = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
+
+      emit(RegisterSuccess());
+    } on FirebaseAuthException catch (ex) {
+      if (ex.code == 'weak-password') {
+        emit(RegisterFailure(errorMessage: 'weak password'));
+      } else if (ex.code == 'email-already-in-use') {
+        emit(RegisterFailure(errorMessage: 'email is already in use'));
+      }
+    } on Exception catch (e) {
+      emit(RegisterFailure(errorMessage: 'something wet wrong try later'));
+    }
+  }
+}
---
+++ lib/views/cubits/auth_cubit/auth_state.dart
@@ -0,0 +1,30 @@
+part of 'auth_cubit.dart';
+
+@immutable
+sealed class AuthState {}
+
+final class AuthInitial extends AuthState {}
+
+final class RegisterInitial extends AuthState {}
+
+final class RegisterLoading extends AuthState {}
+
+final class RegisterSuccess extends AuthState {}
+
+final class RegisterFailure extends AuthState {
+  String errorMessage;
+
+  RegisterFailure({required this.errorMessage});
+}
+
+class LoginInitial extends AuthState {}
+
+class LoginSuccess extends AuthState {}
+
+class LoginLoading extends AuthState {}
+
+class LoginFailure extends AuthState {
+  String errorMessage;
+
+  LoginFailure({required this.errorMessage});
+}
