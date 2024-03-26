044a2d77b557d1c4c11f262655672c665d941623
abduustn <115055703+abduustn@users.noreply.github.com>
2024-03-26 05:13:31 -0700

    Refactor ChatPage widget structure
    Reorganized the ChatPage widget structure for better readability and
    maintainability. Moved message handling logic to appropriate sections.

--- lib/views/chat_page.dart
+++ lib/views/chat_page.dart
@@ -9,98 +9,77 @@ class ChatPage extends StatelessWidget {
 
   final _controller = ScrollController();
 
-  final TextEditingController controller = TextEditingController();
+  List<Message> messagesList = [];
 
-  final CollectionReference messages =
-      FirebaseFirestore.instance.collection(kMessagesCollection);
+  final TextEditingController controller = TextEditingController();
 
   ChatPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     var email = ModalRoute.of(context)!.settings.arguments;
-    return StreamBuilder<QuerySnapshot>(
-        stream: messages.orderBy(kCreatedAt, descending: true).snapshots(),
-        builder: (cotext, snapshot) {
-          // snapshot.data;
 
-          if (snapshot.hasData) {
-            List<Message> messagesList = [];
-
-            for (int i = 0; i < snapshot.data!.docs.length; i++) {
-              messagesList.add(Message.fromJson(snapshot.data!.docs[i]));
-            }
-            return Scaffold(
-              appBar: AppBar(
-                automaticallyImplyLeading: false,
-                backgroundColor: kPrimaryColor,
-                title: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset(
-                      klogo,
-                      height: 50,
-                    ),
-                    const Text('Chat'),
-                  ],
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: kPrimaryColor,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              klogo,
+              height: 50,
+            ),
+            const Text('Chat'),
+          ],
+        ),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+                reverse: true,
+                controller: _controller,
+                itemCount: messagesList.length,
+                itemBuilder: (context, index) {
+                  return messagesList[index].id == email
+                      ? ChatBuble(
+                          message: messagesList[index],
+                        )
+                      : ChatBubleForFriend(
+                          message: messagesList[index],
+                        );
+                }),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: TextField(
+              controller: controller,
+              onSubmitted: (data) {
+                controller.clear();
+                _controller.animateTo(0,
+                    duration: const Duration(seconds: 1),
+                    curve: Curves.fastOutSlowIn);
+              },
+              decoration: InputDecoration(
+                hintText: 'Send Message',
+                suffixIcon: const Icon(
+                  Icons.send,
+                  color: kPrimaryColor,
                 ),
-                centerTitle: true,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16),
+                  borderSide: const BorderSide(color: kPrimaryColor),
+                ),
               ),
-              body: Column(
-                children: [
-                  Expanded(
-                    child: ListView.builder(
-                        reverse: true,
-                        controller: _controller,
-                        itemCount: messagesList.length,
-                        itemBuilder: (context, index) {
-                          return messagesList[index].id == email
-                              ? ChatBuble(
-                                  message: messagesList[index],
-                                )
-                              : ChatBubleForFriend(
-                                  message: messagesList[index],
-                                );
-                        }),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: TextField(
-                      controller: controller,
-                      onSubmitted: (data) {
-                        messages.add({
-                          kMessage: data,
-                          kCreatedAt: DateTime.now(),
-                          'id': email
-                        });
-
-                        controller.clear();
-                        _controller.animateTo(0,
-                            duration: const Duration(seconds: 1),
-                            curve: Curves.fastOutSlowIn);
-                      },
-                      decoration: InputDecoration(
-                        hintText: 'Send Message',
-                        suffixIcon: const Icon(
-                          Icons.send,
-                          color: kPrimaryColor,
-                        ),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(16),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(16),
-                          borderSide: const BorderSide(color: kPrimaryColor),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            );
-          } else {
-            return const Text('loading...');
-          }
-        });
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
